d6448446676c44fb32214a0cb6d02fb77a767ef1
gloer <gloer@langslet.com>
2020-02-07 09:49:24 +0100

    Added chat

--- superchat/index.js
+++ superchat/index.js
@@ -8,6 +8,23 @@ const messageDiv = document.querySelector("#messageDiv");
 const db = firebase.firestore();
 const chatten = db.collection("chatten");
 
+chatten.onSnapshot(snap => {
+    for( const message of snap.docChanges() ) {
+        
+        if(message.type === "added") {
+            const melding = message.doc.data();
+            messageDiv.innerHTML += `
+                <div id="${message.doc.id}">
+                    <span>${melding.from}: </span>
+                    <span>${melding.text}</span>
+                </div>
+            `;
+        }
+        
+        
+    }
+})
+
 messageForm.onsubmit = (evt) => {
     evt.preventDefault();
 
